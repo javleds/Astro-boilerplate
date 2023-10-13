51b757c8343e8102e4cd7aadd465ad234bc85efb
Javier Ledezma <juls0593@gmail.com>
2023-10-12 21:21:22 -0600

    feat: allow environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,13 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { astroImageTools } from 'astro-imagetools';
 
-// https://astro.build/config
-export default defineConfig({
-  // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  trailingSlash: 'always', // Use to always append '/' at end of url
+import { appUrl, appEnv } from './config';
+import { devEnvironment } from './constants';
+
+const config = {
+  site: appUrl,
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'monokai',
     },
   },
@@ -24,4 +22,10 @@ export default defineConfig({
     robotsTxt(),
     astroImageTools,
   ],
-});
+};
+
+if (appEnv === devEnvironment) {
+  config.base = '.';
+}
+
+export default defineConfig(config);
