640eb327a6df2851d7d270dbaccbc75660fb1ba1
Javier <juls0593@gmail.com>
2023-10-16 22:21:11 -0600

    Create resource and tag components

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { z, defineCollection } from 'astro:content';
 export const availableCollections = {
   blog: 'blog',
   projects: 'projects',
+  resources: 'resources',
 }
 
 const blogCollection = defineCollection({
@@ -33,7 +34,24 @@ const projectCollection = defineCollection({
   })
 });
 
+const resourceCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedAt: z.date(),
+    link: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }).optional(),
+    tags: z.array(z.string()),
+  })
+});
+
+
 export const collections = {
   [availableCollections.blog]: blogCollection,
   [availableCollections.projects]: projectCollection,
+  [availableCollections.resources]: resourceCollection,
 };
